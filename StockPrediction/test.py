119efb9b9b02e993ffeb4c6738b35e46e53fb161
OmarAlmrz <almaraz.omar@hotmail.com>
2024-01-26 16:27:27 +0100

    new commit

--- StockPrediction/test.py
+++ StockPrediction/test.py
@@ -4,7 +4,6 @@ import pandas as pd
 import yfinance as yf
 import datetime as dt 
 import plotly.graph_objs as go 
-
 import time  # to simulate a real time data, time loop
 import plotly.express as px  # interactive charts
 import streamlit as st 
@@ -70,7 +69,6 @@ st.title(f"{STOCK_TICKER} Live Share Price")
 # Interval selection for the stock data
 interval_filter = st.selectbox("Select a time interval:", intervalOptions, key="interval")
 
-print(interval_filter)
 # creating a single-element container
 placeholder = st.empty()
 
@@ -89,61 +87,7 @@ with placeholder.container():
     st.markdown("### Detailed Data View")
     st.dataframe(df, use_container_width=True)
     time.sleep(1)
-        
-# # near real-time / live feed simulation
-# for seconds in range(200):
 
-#     df["age_new"] = df["age"] * np.random.choice(range(1, 5))
-#     df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
-
-#     # creating KPIs
-#     avg_age = np.mean(df["age_new"])
-
-#     count_married = int(
-#         df[(df["marital"] == "married")]["marital"].count()
-#         + np.random.choice(range(1, 30))
-#     )
-
-#     balance = np.mean(df["balance_new"])
-
-#     with placeholder.container():
-
-#         # create three columns
-#         kpi1, kpi2, kpi3 = st.columns(3)
-
-#         # fill in those three columns with respective metrics or KPIs
-#         kpi1.metric(
-#             label="Age ⏳",
-#             value=round(avg_age),
-#             delta=round(avg_age) - 10,
-#         )
-        
-#         kpi2.metric(
-#             label="Married Count 💍",
-#             value=int(count_married),
-#             delta=-10 + count_married,
-#         )
-        
-#         kpi3.metric(
-#             label="A/C Balance ＄",
-#             value=f"$ {round(balance,2)} ",
-#             delta=-round(balance / count_married) * 100,
-#         )
-
-#         # create two columns for charts
-#         fig_col1, fig_col2 = st.columns(2)
-#         with fig_col1:
-#             st.markdown("### First Chart")
-#             fig = px.density_heatmap(
-#                 data_frame=df, y="age_new", x="marital"
-#             )
-#             st.write(fig)
-            
-#         with fig_col2:
-#             st.markdown("### Second Chart")
-#             fig2 = px.histogram(data_frame=df, x="age_new")
-#             st.write(fig2)
-
-#         st.markdown("### Detailed Data View")
-#         st.dataframe(df)
-#         time.sleep(1)
+"""
+TODO: use data to display it in real time as a test
+"""
